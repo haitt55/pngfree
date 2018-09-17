ff947d4513a672b1ef989a5f3123034c918a01ca
anhltk <anhltk@rikkeisoft.com>
2018-09-17 17:36:01 +0700

    fix route for admin

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('admin/dashboard','AdminController@index')->name('admin.dashboard');
+Route::get('admin/list-users','AdminController@listUser')->name('admin.listUser');
+Route::post('admin/changeStatus','AdminController@changeStatus')->name('admin.changeStatus');
+Route::post('admin/deleteUser','AdminController@deleteUser')->name('admin.deleteUser');
+Route::get('admin','Admin\LoginController@showLoginForm')->name('admin.login');
+Route::post('admin','Admin\LoginController@login');
+Route::post('admin-password/email','Admin\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+Route::get('admin-password/reset','Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+Route::post('admin-password/reset','Admin\ResetPasswordController@reset');
+Route::get('admin-password/reset/{token}','Admin\ResetPasswordController@showResetForm')->name('admin.password.reset');
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::view('/about.html', 'pages.about');
@@ -52,15 +62,6 @@ Route::group([
     Route::view('/reset-password/success', 'guest.reset_password', ['status' => 1]);
 });
 
-Route::get('admin/dashboard','AdminController@index')->name('admin.dashboard');
-Route::get('admin/list-users','AdminController@listUser')->name('admin.listUser');
-Route::post('admin/changeStatus','AdminController@changeStatus')->name('admin.changeStatus');
-Route::post('admin/deleteUser','AdminController@deleteUser')->name('admin.deleteUser');
-Route::get('admin','Admin\LoginController@showLoginForm')->name('admin.login');
-Route::post('admin','Admin\LoginController@login');
-Route::post('admin-password/email','Admin\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
-Route::get('admin-password/reset','Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
-Route::post('admin-password/reset','Admin\ResetPasswordController@reset');
-Route::get('admin-password/reset/{token}','Admin\ResetPasswordController@showResetForm')->name('admin.password.reset');
+
 
 Auth::routes();
